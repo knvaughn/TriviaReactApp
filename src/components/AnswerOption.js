6ccd3bfb6acd1d8b103a2fb5aad3f391247c68da
Kristn Vaughn <vaughn.kristin.n@gmail.com>
2019-12-27 15:31:07 -0500

    styling setup

--- src/components/AnswerOption.js
+++ src/components/AnswerOption.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/AnswerOption.css';
 
 function AnswerOption(props) {
   return (
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/Results.css';
 
 function Results(props) {
   return (
