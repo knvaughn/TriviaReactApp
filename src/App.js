5b7e9c4feac0354b16db5b2e4184dad4fdf6593b
Kristn Vaughn <vaughn.kristin.n@gmail.com>
2019-12-27 14:15:47 -0500

    get and shuffle answer options method

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
     };
 
     this.startTrivia = this.startTrivia.bind(this);
+    this.getAnswerOptions = this.getAnswerOptions.bind(this);
   }
 
   startTrivia() {
@@ -22,6 +23,20 @@ class App extends Component {
       showTrivia: true,
     });
   }
+
+  getAnswerOptions(questions) {
+    var currentQuestion = questions[this.state.counter];
+    var answers = currentQuestion.incorrect_answers;
+    answers.push(currentQuestion.correct_answer);
+    function shuffle(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+    shuffle(answers);
+    return answers;
+  };
 
   componentDidMount() {
     var questions = [
@@ -66,7 +81,8 @@ class App extends Component {
       this.setState({ 
         questions: questions,
         questionTotal: questions.length,
-        answer: questions[0].correct_answer
+        answer: questions[0].correct_answer,
+        answerOptions: this.getAnswerOptions(questions)
       });
   }
 
@@ -80,7 +96,7 @@ class App extends Component {
               counter={this.state.counter} 
               questionTotal={this.state.questionTotal} 
               question={this.state.questions[this.state.counter].question}
-              answerOptions={['one', 'two', 'three']}
+              answerOptions={this.state.answerOptions}
             /> :
             <div className="landing">
               <h1>Trivia</h1>
