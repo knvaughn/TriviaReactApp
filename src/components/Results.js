c7dd6bdee18adda9631a1edc84e0d2de34398fb3
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2020-11-25 01:43:19 -0500

    build fix

--- src/components/Results.js
+++ src/components/Results.js
@@ -9,8 +9,6 @@ function Results(props) {
         <div className="modalInner">
             <h2 className="results">Results</h2>
             <p className="score">{props.score} / {props.questionTotal}</p>
-            {props.score/props.questionTotal<.5 ? 
-            <p className="insult" dangerouslySetInnerHTML={{ __html: props.insult }}></p> : null}
             <button className="btn-secondary" onClick={props.reset}>Play Again</button>
         </div>
       </div>
